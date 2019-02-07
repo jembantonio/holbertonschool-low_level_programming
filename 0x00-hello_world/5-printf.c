a80e2a4a74fdcec3639def398a84f5cf6a1c0412
Your Name <you@example.com>
2019-02-07 03:08:16 +0000

    initial commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,11 +8,7 @@
 
 int main(void)
 {
-	char str1[51];
-
-	strcpy(str1, "\"Programming is like building a multilingual puzzle");
-
-	puts(str1);
+	printf("with proper grammar, but the outcome is a piece of art,");
 
 	return (0);
 }
