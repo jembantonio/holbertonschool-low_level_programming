987d8bdebafbe4120379228cb9e50ed7da0e6b86
Your Name <you@example.com>
2019-02-07 02:44:29 +0000

    betty style changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
-int main ()
+/**
+* main - Entry Point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
 {
 	char str1[51];
-	
+
 	strcpy(str1, "\"Programming is like building a multilingual puzzle");
-	
+
 	puts(str1);
 
-	return(0);
+	return (0);
 }
