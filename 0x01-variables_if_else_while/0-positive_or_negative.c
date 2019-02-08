70e755b8a5d724dc4ee34261923e85c33479cefc
Your Name <you@example.com>
2019-02-08 02:23:56 +0000

    betty style fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,13 +5,13 @@
 /**
 * main - Entry Point
 *
-* Description: This program will assign a random number 
+* Description: This program will assign a random number
 * to the variable n each time it is executed
 *
 * Return: Always 0 (Success)
 */
 
-int main (void)
+int main(void)
 {
 	int n;
 
