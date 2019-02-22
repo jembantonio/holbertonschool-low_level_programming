ad861ca53449b64c114c615d67a149bd61447023
Your Name <you@example.com>
2019-02-22 03:56:23 +0000

    betty style fix

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -2,7 +2,7 @@
 
 /**
 * leet - a function that encodes a string into 1337
-* @c: string to be converted
+* @s: string to be converted
 *
 * Return: 1337 string
 */
@@ -24,5 +24,5 @@ char *leet(char *s)
 		}
 	}
 
-	return(s);
+	return (s);
 }
