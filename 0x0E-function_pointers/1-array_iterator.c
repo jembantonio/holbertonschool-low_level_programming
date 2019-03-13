f197487594a3d4d59521d1da17f840f6b2041c69
Your Name <you@example.com>
2019-03-13 18:56:29 +0000

    initial commit

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator - a function that executes a function given as a parameter
+* on each element of an array
+* @array: pointer to array
+* @size: size of the array
+* @action: function pointer
+*
+* Return: void
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+* int_index - a function that searches for an integer
+* @array - pointer to array to search
+* @size - number of elements in a array
+* @cmp - pointer to the function to compare values
+*
+* Return: i or -1 if no element matches or size is less than 0
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
