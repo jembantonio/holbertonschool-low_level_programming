f974698c2e73b08cb229dd3fc2805313f6a6410d
Your Name <you@example.com>
2019-03-14 03:52:00 +0000

    changed return value of ops function

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -19,10 +19,9 @@ int (*get_op_func(char *s))(int, int)
 	};
 
 	int i;
+	
+	for (i = 0; i < 5 && *s != *(ops[i].op; i++)
+		;
 
-	for (i = 0; i < 5; i++)
-		if (*s == ops[i].op[0])
-			return (ops[i].f);
-
-	return (NULL);
+	return (ops[i].f);
 }
