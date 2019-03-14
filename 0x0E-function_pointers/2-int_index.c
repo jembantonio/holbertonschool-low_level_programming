83e026e7a8593751922da870f77fb8e0c0a09980
Your Name <you@example.com>
2019-03-14 03:20:31 +0000

    syntax error and betty style fix

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -2,9 +2,9 @@
 
 /**
 * int_index - a function that searches for an integer
-* @array - pointer to array to search
-* @size - number of elements in a array
-* @cmp - pointer to the function to compare values
+* @array: pointer to array to search
+* @size: number of elements in a array
+* @cmp: pointer to the function to compare values
 *
 * Return: i or -1 if no element matches or size is less than 0
 */
@@ -13,6 +13,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (!array || !cmp)
+		return (-1);
 	if (size <= 0)
 		return (-1);
 
