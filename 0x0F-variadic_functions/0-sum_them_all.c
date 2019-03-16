30242d8fde1647190fe68a5a45d9fc458556735b
Your Name <you@example.com>
2019-03-16 05:40:44 +0000

    initial commit

---
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+* sum_them_all - a function that returns the sum of all its parameters
+* @n: number of parameters
+*
+* Return: Sum of all parameters, 0 if n == 0
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i, sum;
+
+	va_start(args, n);
+
+	sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+
+	va_end(args);
+	return (sum);
+}
---
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
