dbd6a9ecb8f77c22a87f8b9c9ca5951885ebcb81
Your Name <you@example.com>
2019-03-18 04:08:31 +0000

    initial commit with updated header

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,105 @@
+#include "variadic_functions.h"
+
+/**
+* print_all - a function that prints anything
+* @format: a list of types of arguments passed to the function
+*
+* Return: void
+*/
+
+void print_all(const char * const format, ...)
+{
+	printall_t list[] = {
+		{"c", print_c},
+		{"i", print_i},
+		{"f", print_f},
+		{"s", print_s},
+		{NULL, NULL}
+	};
+
+	int i, j;
+	char *separator;
+	va_list args;
+
+	j = 0;
+	separator = "";
+
+	va_start(args, format);
+
+
+	for (i = 0; format && format[i]; i++)
+	{
+		while (list[j].c != NULL)
+		{
+			if (format[i] == list[j].c[0])
+			{
+				printf("%s", separator);
+				list[j].function(args);
+				separator = ",";
+			}
+			j++;
+		}
+		j = 0;
+	}
+	putchar('\n');
+
+	va_end(args);
+}
+
+/**
+* print_c - function that prints a char
+* @args: argument list
+*
+* Return: void
+*/
+
+void print_c(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+* print_i - a function that prints an int
+* @args: argument list
+*
+* Return: void
+*/
+
+void print_i(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+* print_f - a function that prints a float
+* @args: argument list
+*
+* Return: void
+*/
+
+void print_f(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+* print_s - a function that prints a string
+* @args: argument list
+*
+* Return: void
+*/
+
+void print_s(va_list args)
+{
+	char *s;
+
+	s = va_arg(args, char *);
+
+	if (s != NULL)
+	{
+		printf("%s", s);
+		return;
+	}
+	printf("(nil)");
+}
+
--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -8,5 +8,25 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_c(va_list args);
+void print_i(va_list args);
+void print_f(va_list args);
+void print_s(va_list args);
+
+/**
+* struct list - a struct that points to a char * as a first parameter
+* and a function pointer as its second
+* @c: char pointer to match
+* @function: function pointer
+*/
+
+typedef struct list
+{
+	char *c;
+	void (*function)(va_list);
+} printall_t;
+
+
 
 #endif
