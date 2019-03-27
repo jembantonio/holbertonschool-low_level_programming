930df1c50be4aa939159b81f0efcb89afe683a4f
Your Name <you@example.com>
2019-03-27 04:48:24 +0000

    betty style fix and initialized i to 0

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -10,11 +10,11 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
 	for (i = 0; i < index; i++)
 		head = head->next;
 
 	return (head);
-	
+
 }
