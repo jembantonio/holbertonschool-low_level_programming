57c44f8413d3e51e1941591b47c38d5294ca9455
Your Name <you@example.com>
2019-03-27 04:39:05 +0000

    added else statement and betty style fix

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-* free_listint2 - a function that frees a linked list and 
+* free_listint2 - a function that frees a linked list and
 * sets the head to NULL
 * @head: a pointer to the address of the start of the linked list
 *
@@ -11,11 +11,14 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+	int i;
 
-	while (*head)
+	if (*head)
 	{
 		temp = (*head)->next;
 		free(*head);
 		(*head) = temp;
-	}	
+	}
+
+	return;
 }
