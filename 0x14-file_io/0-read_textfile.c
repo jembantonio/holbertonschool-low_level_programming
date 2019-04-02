a9427097db100719b7220f8c66901b9cd1e9c0ef
Your Name <you@example.com>
2019-04-02 19:16:17 +0000

    initial commit

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+
+/**
+* read_textfile - a function that reads a text file and prints it
+* to the POSIX standard output
+* @filename: a pointer to the filename to read
+* @letters: the number of letters it should read and print
+*
+* Return: number of letters it could read and print
+* 0 if file cannot be opened, read, or NULL, if write fails
+* return 0
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	
+	int fd, wr;
+	ssize_t bytes;
+	char *buffer;
+
+	/* allocate letters */
+	buffer = malloc(sizeof(char) * letters);
+	
+	/* check if malloc fails */
+	if (!buffer)
+		return (0);
+
+	if (filename)
+	{
+		/* opens file w/ R+W */
+		fd = open(filename, O_RDONLY);
+		/* check error during open */
+		if (fd == -1)
+			return (0);
+
+		/* reads text into buffer from file */
+		bytes = read(fd, buffer, letters);
+
+		/* write to stdout from buffer */
+		wr = write(STDOUT_FILENO, buffer, bytes);
+
+
+		close(fd);
+		free(buffer);
+		return(wr);
+	}
+
+	return (0);
+}
---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+* create_file - a function that creates a file
+* @filename: name of the file to create
+* @text_content: NULL terminated string to write the file
+*
+* Return: 1 on success, -1 on failure
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len, content;
+
+	/* strlen function */
+	len = 0;
+
+	while (text_content[len])
+		len++;
+
+
+	/* error checks */
+	if (!filename)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	/* creates file w/ R+W for U */
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	/* write the content to a file */
+	content = write(fd, text_content, len);
+	if (content == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
