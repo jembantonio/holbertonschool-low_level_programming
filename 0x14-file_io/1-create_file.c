64ee223340fb0344b0bc1faffe228f2771b2cf3f
Your Name <you@example.com>
2019-04-03 04:52:41 +0000

    added one error check for 1 and push for 2

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -23,13 +23,16 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
-
 	/* creates file w/ R+W for U */
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
 
 	/* write the content to a file */
 	content = write(fd, text_content, len);
