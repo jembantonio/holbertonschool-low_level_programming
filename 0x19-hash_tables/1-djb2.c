3fac4c590289a983902e7b321404f19b24ccc7cc
jembantonio <jembantonio@gmail.com>
2019-06-04 23:05:10 +0000

    betty style fix

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -2,15 +2,16 @@
 
 /**
  * hash_djb2 - a hash function implementing the djb2 algorithm
+ * @str: value to be converted to an index
  *
- *
+ * Return: the index of the value in the array
  */
 
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash = 5381;
 	int c;
-	    
+
 	while ((c = *str++))
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
