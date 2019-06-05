1488ed7552bc2cb029a0b9835d5eb273d257428b
jembantonio <jembantonio@gmail.com>
2019-06-05 00:55:39 +0000

    added case for *key is NULL

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht)
 		return (0);
-	if (!key || !value)
+	if (!key || !value || !(*key))
 		return (0);
 
 	/* creating new key value pair */
