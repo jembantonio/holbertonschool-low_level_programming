b02da490b0ede38723435a1fd84eb2a5c7cfb281
jembantonio <jembantonio@gmail.com>
2019-06-18 20:58:31 +0000

    betty check header

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -6,12 +6,12 @@
 #include <string.h>
 
 /**
- *  * struct listint_s - Doubly linked list node
- *   *
- *    * @n: Integer stored in the node
- *     * @prev: Pointer to the previous element of the list
- *      * @next: Pointer to the next element of the list
- *       */
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
 {
 	const int n;
@@ -25,5 +25,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void swap(int *a, int *b);
 
 #endif
